774862ecc15e743aa415653591656cfe31eb0deb
Sandrava Philips <lacasandra1@gmail.com>
2019-10-11 08:37:21 +0100

    built my formReducer

--- smurfs/src/state/reducers.js
+++ smurfs/src/state/reducers.js
@@ -5,3 +5,17 @@ const initialFormValues = {
     age: 0,
     height: ''
 }
+
+function formReducer(state= initialFormValues, action) {
+    switch(action.type) {
+        case types.ON_INPUT_CHANGE:
+            return {
+                ...state,
+                [action.payload.name]: action.payload.value,
+            };
+        case types.ON_FORM_SUBMIT:
+            return state;
+        default:
+            return state;
+    }
+}
